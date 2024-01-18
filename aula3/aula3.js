828b5c44730e74829a0de3a4b5fec4a781ecc0ed
RichardJanebo <richardjanebo@gmail.com>
2024-01-18 07:39:43 -0300

    aula 3

---
+++ aula3/aula3.js
@@ -0,0 +1,16 @@
+"use strict"
+
+function vernome(){
+    let nome = "Bruno"
+    if (true){
+        console.log(`Variavel declarada no if dentro da função ${nome}`)
+    }
+    console.log(`Variavel declarada dentro da função ${nome}`)
+
+}
+vernome()
+console.log(`Variavel declarada fora do escopo da função ${nome}`) 
+
+const numer = 10 
+var soma = numer + 1
+console.log(soma)
