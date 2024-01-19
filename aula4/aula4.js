b546d33524a60baf15e051b95419d38d9f048ac8
RichardJanebo <richardjanebo@gmail.com>
2024-01-19 07:04:09 -0300

    Operadores aritimeticos

--- aula4/aula4.js
+++ aula4/aula4.js
@@ -1,6 +1,10 @@
-let nun1 = 3
+let nun1 = 4
 let nun2 = 2
-let res = nun1 % nun2
+let res = nun1%2
 
-console.log(res)
+if (res == 0){
+    console.log('Esse numero é par', res)
+}else{
+    console.log("esse numero é impar", res)
+}
 
