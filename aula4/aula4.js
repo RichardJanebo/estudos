262e2409bae993f47925f3b730062a44a83b1ad1
RichardJanebo <richardjanebo@gmail.com>
2024-01-19 08:00:32 -0300

    Operadores relacionais

--- aula4/aula4.js
+++ aula4/aula4.js
@@ -1,10 +1,13 @@
 let nun1 = 4
 let nun2 = 2
-let res = nun1%2
+let res = 4
+res++
+res++
 
-if (res == 0){
+/*if (res == 0){
     console.log('Esse numero é par', res)
 }else{
     console.log("esse numero é impar", res)
-}
+}*/
+console.log(res)
 
