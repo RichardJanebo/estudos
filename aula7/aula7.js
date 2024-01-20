f1515d41bec94e8c29af754f9027324bd2832623
RichardJanebo <richardjanebo@gmail.com>
2024-01-20 11:43:15 -0300

    Aula 7 operadores logicos

---
+++ aula7/aula7.js
@@ -0,0 +1,15 @@
+let n1,n2,n3,n4,n5
+n1 = 10 
+n2 = 15
+n3 = 5
+n4 = 30
+n5 = 50
+
+
+
+if(n4 < n1){
+    console.log(`A variavel n4 ${n4} é maior que a variavel n1 ${n1}`)
+}else if((n3 < n1) && (n4 > n2) || (n3 == n4)){
+    console.log("Estrutura verdadeira")
+}
+console.log(12 * 3 + 4 - 8 / 2 % 3)
