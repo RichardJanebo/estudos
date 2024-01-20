b74f10a629ccb97e134645818a8f0dabe1172b8a
RichardJanebo <richardjanebo@gmail.com>
2024-01-20 19:35:29 -0300

    Aula 10 operadores ternarios

---
+++ aula10/aula10.js
@@ -0,0 +1,12 @@
+
+
+
+let n = 10
+let x = ( n%2 == 0 ? imprimir() : "Par")
+
+
+function imprimir(){
+    console.log("Isso só ta aparecendo por que a condição do operador ternario se mostrou verdadeira")
+}
+
+//Teste logico para saber se é 0 ou 1 ? se retorno verdadeiro : se retorno falso
