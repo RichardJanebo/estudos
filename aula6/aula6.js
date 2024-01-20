b57918bdd5eaf624902ed9b2266804928a2b338c
RichardJanebo <richardjanebo@gmail.com>
2024-01-20 10:53:26 -0300

    Aula 6

---
+++ aula6/aula6.js
@@ -0,0 +1,11 @@
+/*let nome = prompt("Digite seu nome")
+console.log(nome)*/
+let num1 = 10 
+let num2 = 5
+console.log(!(num1 != num2))
+
+if (navigator.userAgent.match(/Android/i)){ //Define pelo java script em qual dispositivo esta rodando o codigo
+    console.log("celular")
+}else{
+    console.log("pc")
+}
