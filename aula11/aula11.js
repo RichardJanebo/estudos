ff5dd2d66fcdee9728843ac1528a0403c9909680
RichardJanebo <richardjanebo@gmail.com>
2024-01-21 11:55:31 -0300

    Aula 10 com o operador unario Typeof

---
+++ aula11/aula11.js
@@ -0,0 +1,9 @@
+var a = 10
+var b = "10"
+var c = a === b
+var d = {nome:"Richard"}
+
+console.log(`Valor ${a} e o tipo é ${typeof(a)}`)
+console.log(`Valor ${b} e o tipo é ${typeof(b)}`)
+console.log(`Valor ${c} e o tipo é ${typeof(c)}`)
+console.log(`Valor ${d} e o tipo é ${typeof(d)}`)
