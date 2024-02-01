16032e84a38d6a5a2a26b33ec1d8811e6194dbb4
RichardJanebo <richardjanebo@gmail.com>
2024-02-01 08:02:15 -0300

    addeventlistener

--- aula33/aula33.js
+++ aula33/aula33.js
@@ -1,2 +1,6 @@
-const todasAsDivs = [...document.querySelectorAll("div")]
+const todasAsDivs = [...document.querySelectorAll("div[class]")]
+const cursosC1 = [...document.querySelectorAll(".c1,.c2")]
+const curso = [...document.querySelectorAll(".c2")]
 console.log(todasAsDivs)
+console.log(cursosC1)
+console.log(curso)
---
+++ aula34/aula34.js
@@ -0,0 +1,6 @@
+const curso1 = document.querySelectorAll('.c1')
+
+function msg(){
+    alert("ola mundo")
+}
+curso1.addEventListener('click', msg)
