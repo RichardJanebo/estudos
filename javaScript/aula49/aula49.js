01f7494bced2a56a7c7d1ca0abb89e1397b09cab
RichardJanebo <richardjanebo@gmail.com>
2024-02-14 22:43:25 -0300

    Entendendo mais a fundo metodo some na teoria

--- javaScript/aula49/aula49.js
+++ javaScript/aula49/aula49.js
@@ -3,12 +3,19 @@ const caixaPesquisar = document.querySelector("#btnPesquisar")
 const textPesquisar = document.querySelector("#text_pesquisar")
 const resultado = document.querySelector("#resultado")
 
-const arrey_numerico = [21, 20, 10, 26, 55, 98]
+const arrey_numerico = [1, 2, 13, 12, 5, 10,19]
 caixaArrey.innerHTML = arrey_numerico
 
 caixaPesquisar.addEventListener("click",()=>{
-    const verot = arrey_numerico.some((el)=>{
-        el > 18
+    const verot = arrey_numerico.some((el,i)=>{
+        if ( el <= 18){
+            resultado.innerHTML=`O valor que não esta em conformidade é o ${i}`
+        }
+        return el > 18
+        
     })
-    console.log(verot)
+    if (verot == true){
+
+        resultado.innerHTML=`O valor esta em conformidade`
+    }
 })
