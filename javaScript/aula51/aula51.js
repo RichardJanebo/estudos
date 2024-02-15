6ea68b019685426c447edf525addf1dab69648c0
RichardJanebo <richardjanebo@gmail.com>
2024-02-15 07:54:58 -0300

    Aprendendo mais sobre iteradores

---
+++ javaScript/aula51/aula51.js
@@ -0,0 +1,8 @@
+// const caixaArrey = document.querySelector("#arrey")
+// const caixaPesquisar = document.querySelector("#btnPesquisar")
+// const textPesquisar = document.querySelector("#text_pesquisar")
+// const resultado = document.querySelector("#resultado")
+
+const arrey_numerico = [1,2,3,4,5,6,7]
+const it_valoreres  = arrey_numerico[Symbol.iterator]()
+console.log(it_valoreres.next())
