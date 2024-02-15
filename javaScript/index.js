01d9cf284898624629d835417d3bbea0eb50cfd4
RichardJanebo <richardjanebo@gmail.com>
2024-02-14 22:57:04 -0300

    Revisão for

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,4 +1,18 @@
-const divMaior = document.createElement("div")
-const body = document.getElementsByTagName("body")
+//  
 
-body.appendChild(divMaior)
+for (i= 0 ; i < 10; i++){
+    console.log("ola")
+}
+const arrey_numerico = [12, 2, 10, 6, 5, 98]
+const receber =arrey_numerico.some((el,i)=>{
+    if(el < 18){
+        console.log(`O elemento ${i} não esta em conformidade`)
+    }
+    return el > 18
+
+   
+})
+if ( receber == true){
+    console.log("Tudo certo")
+
+}
