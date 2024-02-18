9be8270272b10c408eee4a7ac67d9d64540a7b4f
RichardJanebo <richardjanebo@gmail.com>
2024-02-18 19:16:09 -0300

    Refatoramento da calculadora com shothands

--- javaScript/aula53/aula53.js
+++ javaScript/aula53/aula53.js
@@ -3,70 +3,33 @@ const adicao = document.querySelector("#adicao")
 const subtracao = document.querySelector("#subtracao")
 const divisao = document.querySelector("#divisao")
 const resultado = document.querySelector("#resultado")
-const valor1 =document.querySelector("#valor1")
-const valor2 =document.querySelector("#valor2")
+
 
 
 
 const calcular = [
-    (el)=>{
-        let soma = 0
-        for (v of el){
-            soma += v 
-
-        }
-        return soma
+    ()=>{
+        const numeros =[document.querySelector("#valor1").value,document.querySelector("#valor2").value]
+        resultado.value = Number(numeros[0]) +  Number(numeros[1])
+       
     },
-    (el)=>{
-        let menos = 0
-        for (v of el){
-            menos = v 
-            
-
-        }
-        return menos
+    ()=>{
+        const numeros =[document.querySelector("#valor1").value,document.querySelector("#valor2").value]
+        resultado.value = Number(numeros[0]) - Number(numeros[1])
     },
-    (el)=>{
-        let vezez = 1
-        for (v of el){
-            vezez *= v 
-
-        }
-        return vezez
+    ()=>{
+        const numeros =[document.querySelector("#valor1").value,document.querySelector("#valor2").value]
+        resultado.value = Number(numeros[0]) *  Number(numeros[1])
     },
-    (el)=>{
-        let dividi = 1
-        for (v of el){
-            dividi /= v 
-
-        }
-        return dividi
+    ()=>{
+        const numeros =[document.querySelector("#valor1").value,document.querySelector("#valor2").value]
+        resultado.value = Number(numeros[0]) /  Number(numeros[1])
     }
 ]
 
-const tipos = [adicao,subtracao,divisao,multipicacao]
+adicao.addEventListener("click",calcular[0])
+subtracao.addEventListener("click",calcular[1])
+multipicacao.addEventListener("click",calcular[2])
+divisao.addEventListener("click",calcular[3])
 
-tipos.map((el=>{
-    el.addEventListener("click",()=>{
-        const valor01 = parseInt(valor1.value)
-        const valor02 = parseInt(valor2.value)
-        const valores = [valor01,valor02]
-        
-        if ( el == adicao){
-             console.log(calcular[0](valores))
-        }else if(el == subtracao){
-            console.log(calcular[1](valores))
 
-        }else if(el == multipicacao){
-            console.log(calcular[2](valores))
-        }else if(el == divisao){
-            console.log(calcular[3](valores))
-        }
-
-           
-            
-        
-    })
-    
-
-}))
