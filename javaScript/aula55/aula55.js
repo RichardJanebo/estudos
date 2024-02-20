0b651f246eba50ef00fe7aa45479e85919b0fdec
RichardJanebo <richardjanebo@gmail.com>
2024-02-20 07:59:02 -0300

    Coleção Set

---
+++ javaScript/aula55/aula55.js
@@ -0,0 +1,20 @@
+// const caixa = document.getElementById("caixa")
+
+// const setColection = new Set()
+
+// setColection.add("musica")
+// setColection.add("comida")
+// setColection.add("musica")
+
+
+
+// setColection.forEach((el)=>{
+    
+//     console.log(el);
+// })
+// console.log(setColection.entries());
+
+let num = [10,20,30,40,10,20,22,36,22]
+
+let arr = [...new Set(num)]
+console.log(arr.length)
