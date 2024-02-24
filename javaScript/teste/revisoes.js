157846e40f5f0f4274bb16e90af4307ab560e695
RichardJanebo <richardjanebo@gmail.com>
2024-02-24 13:32:43 -0300

    Contador de vogal

--- javaScript/teste/revisoes.js
+++ javaScript/teste/revisoes.js
@@ -1,12 +1,22 @@
-function palavra(el){
-    let p = el.split("").reverse().join("")
+// Contador de vogais 
 
-    console.log(p)
-    console.log(el)
-    return el === p
-    
+let vogal = ["a","e","i","o","u"]
 
+let palvra = "JavaScript"
 
+function vowelConter(palvra){
+    let letras = palvra.split("")
+    let contador = 0
+
+    for (j of letras){
+        for(n of vogal)
+        if (j === n){
+            contador ++
+
+        }
+    }
+    return contador
 }
+console.log(vowelConter(palvra))
 
-console.log(palavra("ovo"))
+
