953281b221791fb5a2b0ac9c023244a1adfd915a
RichardJanebo <richardjanebo@gmail.com>
2024-02-24 13:17:18 -0300

    Criando verificador de palidromo

--- javaScript/teste/revisoes.js
+++ javaScript/teste/revisoes.js
@@ -1,51 +1,12 @@
-//Algoritimo de media 
+function palavra(el){
+    let p = el.split("").reverse().join("")
 
-const div   = document.querySelector("#div1")
-const input = document.createElement("input")
-const p     = document.createElement("p") 
-const calcular = document.createElement("button")
-const inserir = document.createElement("button")
-const p2 = document.createElement("p")
-
+    console.log(p)
+    console.log(el)
+    return el === p
+    
 
 
+}
 
-input.setAttribute("type","number")
-p.innerHTML="Resultado"
-calcular.innerHTML="Calcular"
-inserir.innerHTML="inserir"
-
-div.appendChild(input)
-div.appendChild(inserir)
-div.appendChild(p2)
-div.appendChild(calcular)
-div.appendChild(p)
-
-let numeroDigitado = []
-Number(numeroDigitado)
-inserir.addEventListener("click",()=>{
-
-    numeroDigitado.push(input.value)
-    p2.innerHTML=numeroDigitado
-})
-
-calcular.addEventListener("click",()=>{
-    let soma = 0
-    let indice = 1
-    numeroDigitado.map((el,i)=>{
-        
-        soma = soma + Number(el) 
-        indice = i
-        
-    })
-    indice = indice + 1
-    console.log(soma)
-    console.log(indice)
-
-    let calc = soma / indice
-
-    p.innerHTML=`A media dos numeros é ${calc.toFixed(2)}`
-})
-
-
-
+console.log(palavra("ovo"))
