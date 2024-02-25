d3a4e4331519f84dd2702db4acccc4de8e59f648
RichardJanebo <richardjanebo@gmail.com>
2024-02-25 20:17:37 -0300

    Revisão de domingo

--- javaScript/teste/revisoes.js
+++ javaScript/teste/revisoes.js
@@ -1,22 +1,47 @@
-// Contador de vogais 
+// for (i =0 ; i < 10; i++){
+//     console.log(i)
 
-let vogal = ["a","e","i","o","u"]
+//     if (i == 5){
+//         break
+//     }
+// }
+// console.log("depois do loop")
 
-let palvra = "JavaScript"
-
-function vowelConter(palvra){
-    let letras = palvra.split("")
-    let contador = 0
-
-    for (j of letras){
-        for(n of vogal)
-        if (j === n){
-            contador ++
-
-        }
-    }
-    return contador
-}
-console.log(vowelConter(palvra))
+let m = [10,20,30,40,50]
+let m1 = [10,20,30,40]
 
 
+console.log(...m,...m1)
+
+const h1 = document.querySelector("#h1")
+const botao = document.querySelector("#button")
+
+
+const div1 = document.querySelector("#div1")
+div1.addEventListener("click",()=>{
+    h1.classList.toggle("vermelho") 
+    
+} 
+  )
+
+
+
+  div2.appendChild(selecionado)
+
+
+  const selecionado = [...document.getElementsByClassName("vermelho")  ]
+
+  selecionado.map((el)=>{
+   let tae = el.textContent
+})
+
+
+    console.log(selecionado[0])
+  const div2 = document.querySelector("#div2")
+  console.log(div2)
+  botao.addEventListener("click",()=>{
+      // const naoSelecionado = document.querySelector(".curso not(.selecionado)")
+      
+  
+  
+  })
