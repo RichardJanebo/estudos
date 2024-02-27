f41b533a588c09ec52d197efafd23ab262687989
RichardJanebo <richardjanebo@gmail.com>
2024-02-27 07:57:17 -0300

    Aula 59 Objects part III

--- javaScript/aula59/aula59.js
+++ javaScript/aula59/aula59.js
@@ -1,9 +1,63 @@
-const nome  = document.querySelector("#textNome")
-const idade = document.querySelector("#idade")
+const nomes  = document.querySelector("#textNome")
+const idade = document.querySelector("#textidade")
 const botao = document.querySelector("#verificar")
 const main  = document.querySelector("#main")
-const p     = document.createElement("p")
+const p     = document.createElement("div")
 
 main.appendChild(p)
 p.setAttribute("class","paragrafo")
 p.innerHTML="Teste"
+
+
+class pessoa{
+    constructor(pnome,pidade){
+        this.nome = pnome 
+        this.idade = pidade 
+    }
+    getNome(){
+        return this.nome
+    }
+    getIdade(){
+        return this.idade
+    }
+    setNome(nome){
+        this.nome = nome
+    }
+    setIdade(idade){
+        this.idade = idade
+    }
+    info(){
+        return `nome:${this.nome} Idade:${this.idade}`
+    }
+}
+
+let pessoas = []
+
+
+const addPessoas = ()=>{
+    p.innerHTML=""
+    pessoas.map((el)=>{
+        const div = document.createElement("div")
+        div.setAttribute("class","Pessoa")
+        div.innerHTML=`Nome: ${el.getNome()} Idade:${el.getIdade()}`
+
+        p.appendChild(div)
+    })
+    
+
+}
+
+botao.addEventListener("click",()=>{
+    let q1 = new pessoa(nomes.value,idade.value)
+
+    pessoas.push(q1)
+    nomes.value=""
+    idade.value=""
+
+    console.log(pessoas)
+
+    addPessoas()
+
+  
+
+})
