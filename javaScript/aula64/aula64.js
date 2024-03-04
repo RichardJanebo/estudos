d144b82317eac46e8f7774fce5f65e958b0c32db
RichardJanebo <richardjanebo@gmail.com>
2024-03-04 07:26:40 -0300

    Removendo elementos do formulario I

---
+++ javaScript/aula64/aula64.js
@@ -0,0 +1,90 @@
+const radio1 = document.querySelector("#txtradio1")
+const radio2 = document.querySelector("#txtradio2")
+const blind = document.querySelector("#txtblindagem")
+const muni = document.querySelector("#txtmunicao")
+const nome = document.querySelector("#txtname")
+const adicionar = document.querySelector("#adicionar")
+const port = document.querySelector("#txtdoor")
+const paragrafo = document.querySelector("#paragrafo")
+
+
+
+
+
+
+radio1.addEventListener("click", (evt)=>{
+    blind.removeAttribute("disabled")
+    muni.removeAttribute("disabled")
+    blind.value=0
+    muni.value=0
+})
+
+radio2.addEventListener("click", (evt)=>{
+    blind.setAttribute("disabled","disabled")
+    muni.setAttribute("disabled","disabled")
+    blind.value=0
+    muni.value=0
+})
+
+
+class normal{
+    constructor(pnome,pportas){
+        this.nome = pnome
+        this.pportas = pportas
+
+    }
+}
+
+class blindado extends normal{
+    constructor(pnome,pportas,blindagem,municao){
+        super(pnome,pportas)
+        this.minicao = municao
+        this.blindagem = blindagem
+    }
+
+    info(){
+        return `Nome: ${this.nome} Portas:${this.pportas} `
+    }
+}
+
+let arrey_carros = []
+
+arrey_carros.filter
+
+const gerenciarExibicaoDeCarros=()=>{
+    paragrafo.innerHTML=""
+
+    arrey_carros.forEach((c)=>{
+        console.log(c)
+        const div = document.createElement("div")
+        const btn = document.createElement("button")
+        btn.addEventListener("click",(evt)=>{
+            console.log(evt.target.parentNode.dataSet.nome)
+
+        })
+        div.setAttribute("class","carros")
+        div.setAttribute("data-nome",c.nome)
+        div.innerHTML=`Nome:${c.nome} Portas:${c.pportas} Munição:${c.minicao} Blindagem:${c.blindagem}`
+        btn.innerHTML="Remover"
+        paragrafo.appendChild(div)
+        div.appendChild(btn)
+       
+    })
+}
+
+
+adicionar.addEventListener("click",()=>{
+    if(radio1.checked){
+        let c1 = new blindado(nome.value,port.value,blind.value,muni.value)
+        arrey_carros.push(c1)
+    }else{
+        let c2 = new normal(nome.value,port.value)
+        arrey_carros.push(c2)
+    }
+
+    gerenciarExibicaoDeCarros()
+})
+
+
+
+
