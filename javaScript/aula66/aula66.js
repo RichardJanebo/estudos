2ac19cc9b0ca61e813a4112a8f79cf164794dea1
RichardJanebo <richardjanebo@gmail.com>
2024-03-06 07:33:02 -0300

    membros Static

---
+++ javaScript/aula66/aula66.js
@@ -0,0 +1,16 @@
+class npc{
+    static alerta = false
+    constructor(energy){
+        this.energia = energy
+    }
+
+    info(){
+        return `Alert :${npc.alerta} Energia${this.energia} `
+    
+    }
+}
+
+const npc1 = new npc(100)
+
+
+console.log(npc1.info())
