16bad5aa01a34db72594077e6bd34a1a98c56691
RichardJanebo <richardjanebo@gmail.com>
2024-04-07 16:22:45 -0300

    Revisão 07-04-2024

--- javaScript/teste/revisoes.js
+++ javaScript/teste/revisoes.js
@@ -1,21 +1,161 @@
-let n = 10
-let fat = 1
+// let at = document.createElement("div")
+// at.setAttribute("id","Ola")
+const body = document.getElementById("body")
 
-while (n > 1){
-    fat = fat * n
-    console.log(fat)
-    n--
+let  altura = body.offsetHeight
+
+
+console.log(altura)
+
+
+window.addEventListener("resize",()=>{
+    let largura = body.offsetWidth
+    console.log(largura)
+    
+})
+let controls = false
+
+
+
+const Nave =  function(energia){
+    
+    this.energia = energia
+    this.disparos = 10
+    Nave.prototype.vidas = 3 
+    
 }
 
-const aulas = {
-    nome:"Java Script",
-    materia:"objetos",
-    aula:{
-        aula01:"introdução",
-        aula02:"desenvolvimento",
-        aula03:"conclusão"
 
+let c = new Nave(100)
+
+let ener = c.energia
+
+
+
+const f = function(n,m){
+    return(n+m)
+    
+}
+
+console.log(f(10,10))
+
+let numero_1 = 10
+let numero_2 = 3000
+
+console.log(numero_1++)
+
+
+
+
+
+
+let promessa = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        controls = true
+        if(controls == true){
+            resolve("Deu certo")
+        }else{
+            reject("Deu errado")
+        }
+
+       
+        
+    },numero_2)
+
+    
+    
+
+})
+
+
+.then((retorno)=>{
+    console.log(retorno)
+})
+.catch((retorno)=>{
+    console.log(retorno)
+})
+
+
+console.log(10+10)
+
+const sim = ()=>{
+    let n = 10.5
+    console.log(Math.floor(n))
+}
+
+sim()
+
+
+
+
+
+
+let set = new Set()
+
+set.add("palmeiras")
+set.add("corintias")
+set.add("são paulo")
+set.add("flamengo")
+set.add("santos")
+set.add("são paulo")
+set.add("flamengo")
+
+set.delete("palmeiras")
+
+console.log(set)
+
+
+
+class Pessoa{
+    constructor(pnome){
+        this.nome = pnome
+        this.endereço = pnome
+        this.telefone = pnome
     }
 }
 
-console.log(JSON.stringify(aulas))
+const p1 = new Pessoa("Richard")
+const P2 = new Pessoa("Rua das agulhas negras n 813")
+const p4 = new Pessoa(6795552994499929)
+
+console.log(p1.nome)
+console.log(P2.endereço)
+console.log(p4.telefone)
+
+
+
+const map =new Map()
+map.set("curso","javaScript")
+map.set("corrida","Velozes e furiosos")
+
+console.log(map.get("curso"))
+
+
+
+//detectando qual dispositivo esta rodando o codigo 
+const text = document.querySelector("#texto")
+
+const click = document.querySelector("#botao")
+click.addEventListener("click",()=>{
+    if(navigator.userAgent.match(/Android/i)){
+      alert("celular")
+    }else{
+        alert("pc")
+    }
+
+    navigator.clipboard.writeText(text.value)
+    
+
+    
+})
+let j = 10
+
+while(j >= 1){
+    console.log(j)
+    j--
+    if(j == 5){
+        break
+    }
+}
+
+
