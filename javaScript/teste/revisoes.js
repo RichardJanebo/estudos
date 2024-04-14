8fb0f470372b521f796a106613ccd1701d83a71d
RichardJanebo <richardjanebo@gmail.com>
2024-04-14 19:32:01 -0300

    revisões 14/04/2024

--- javaScript/teste/revisoes.js
+++ javaScript/teste/revisoes.js
@@ -1,25 +1,38 @@
-let idade = 0
+// const somaDoisNumeros = (a,b)=>{
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             resolve(a + b)
+//         },2000)
+//         reject("Deu errado")
+//     })
+    
+// }
+
+// somaDoisNumeros(20,20)
+// .catch((resultado)=>{
+//     console.log(resultado)
+// })
 
 
-const name = "Richard"
-const agr = 20
 
-const objd = {
-name,
-agr,
+
+// .then((resposta)=>{
+//     const recebeu = resposta.json().then(body=>
+//         body.slip
+//     )
+
+//     console.log(recebeu)
+  
+  
+// })
+// .catch((resposta)=>{
+//     console.log(resposta)
+// })
+
+async function buscarFrases (){
+    const response = await fetch("https://api.adviceslip.com/advice")
+    .then((r)=>{r.json().then((b)=>{console.log(b.slip.advice)})})
+
 }
 
-
-
-
-const users ={ 
-    zip:{
-        address:{
-            codex:354744
-
-        }
-    },
-}
-
-
-console.log(users.zip?.address?.code  ?? "Não informado")
+buscarFrases()
