f1447dd0730e2a68efdc678b40ba3e839285de96
RichardJanebo <richardjanebo@gmail.com>
2024-04-24 06:47:26 -0300

    importando todos os modulos

---
+++ javaScript/aula94/aula94.js
@@ -0,0 +1,10 @@
+// import getAllCursos, { cursos } from "./cursos94.js"
+
+// import { cursos as c}  from "./cursos94.js"
+
+
+import * as m_cursos from "./cursos94.js"
+
+console.log(m_cursos.cursos)
+console.log(m_cursos.default(1))
+
---
+++ javaScript/aula94/cursos94.js
@@ -0,0 +1,9 @@
+const cursos = ["Java Script","HTml","Css","Java","Php"]
+
+export default function getAllCursos(i){
+    return cursos[i]
+}
+
+
+export {cursos}
+
