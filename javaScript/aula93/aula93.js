17ed1da2188ebffeaf40597d0f77540e9a8b1ef1
RichardJanebo <richardjanebo@gmail.com>
2024-04-24 06:27:47 -0300

    exportação padrão

---
+++ javaScript/aula93/aula93.js
@@ -0,0 +1,7 @@
+import getAllCursos,{ cursos } from "./cursos93.js";
+
+
+
+
+console.log(cursos)
+console.log(getAllCursos(2))
---
+++ javaScript/aula93/cursos93.js
@@ -0,0 +1,10 @@
+const cursos = ["Java Script","HTml","Css","Java","Php"]
+
+const getAllCursos = (i)=>{
+    return cursos[i]
+}
+
+
+export {cursos}
+
+export default getAllCursos
