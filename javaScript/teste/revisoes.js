b205c45952a3652be9160d28eea1b4b6c4b4658b
RichardJanebo <richardjanebo@gmail.com>
2024-04-28 22:47:18 -0300

    revisão swith 28-04-2024

--- javaScript/teste/revisoes.js
+++ javaScript/teste/revisoes.js
@@ -1,18 +1,18 @@
-let nome = "João Da Silva"
+const botao = document.querySelector("#botao")
+const res = document.querySelector("#res")
 
-nome.replace("D","L")  //O primeiro parâmetro é substituido pelo segundo = João La Silva
-nome.slice(0,4)      //Fatia a string entre o primero índice atribuído e o ultimo = João
-nome = nome.split("")    // Separa a string em um arrey, com suas letras = 'J', 'o', 'ã','o'...
-// nome.charAt(5)                  //Mostra qual letra esta no índice que foi atribuído= 'D'
-// nome.toLowerCase()      //Deixa todos os caracteres da string minúsculo = 'joão da silva'
-// nome.toUpperCase()      //Deixa todos os caracteres da string maiúsculo = 'JOÃO DA SILVA'
-// nome.charCodeAt(5)                                   //Retorna o código do caractere = 68
-// nome.indexOf("J")                      //Retorna o índice da primeira letra atribuída = 0
-// nome.repeat(2)//Repete a string na quantidade de vezes atribuda = João Da Silva Jõao D...
-// nome.valueOf()                      //Retorna o valor primitivo da string = João Da Silva
-// nome.search("l")                          //Retorna o índice do caractere pesquisado = 10
-// nome.includes("Silva")    //Verifica se existe o conteudo passado dentro da string = true
-// nome.startsWith("João")   //Verifica se a string começa com os caracteres passados = true
-// nome.endsWith("Silva")   //Verifica se a string termina com os caracteres passados = true
+botao.addEventListener("click",()=>{
+    testedecaso(0)
+})
 
-console.log(nome)
+
+const testedecaso = (e)=>{
+    switch (e){
+        case 1: 
+        res.innerHTML=`Foi digitado o ${e}`
+        break
+    default:
+        res.innerHTML=`Nenhum Parametro identificado`
+    }
+}
+
