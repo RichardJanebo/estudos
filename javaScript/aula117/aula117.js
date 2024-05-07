0675a2a92b1115b2412bc316f6acba229ae34c5d
RichardJanebo <richardjanebo@gmail.com>
2024-05-07 07:40:06 -0300

    Uso do ponto e virgula

---
+++ javaScript/aula117/aula117.js
@@ -0,0 +1,7 @@
+for (i= 1;i<10;i++){ //Ponto e virgula obrigatorio
+    console.log("Bom Dias")
+    if(i == 8){
+        break
+    }
+}
+console.log("Fora do loop")
---
+++ javaScript/aula118/aula118.js
@@ -0,0 +1,15 @@
+//[,(
+let numeros = [10,20,30]; // o proximo item é um [ então precisa do ;
+[n1,n2,n3] = numeros
+console.log(n1)
+
+
+let nome = "Richard"; // o proximo item é um [ então precisa do ;
+["r","i","c","h","a","r","d"].forEach(l=>console.log(l))
+
+let n = null; //O proximo item é um ( então precisa do ;
+(n=10)
+console.log(n)
+
+let i = 10; i = 20 //Caso um avariavel seja alterada na mesma linha 
+console.log(i)
