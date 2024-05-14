f238c358db92a0c641a68c0856ddced02cd528cf
RichardJanebo <richardjanebo@gmail.com>
2024-05-14 08:47:42 -0300

    revisão 14-05-2024

--- javaScript/teste/revisoes.js
+++ javaScript/teste/revisoes.js
@@ -1,7 +1,10 @@
-    import {farofa} from "/moduloTeste1.js";
+let p1=[50,40,30,20,10]
 
-if (farofa) {
-  farofa.map((el) => {
-    console.log(el);
-  });
+let x = p1[p1.length -1]
+console.log(x)
+
+const soma = (x,y)=>{
+  return x + y
 }
+
+console.log(soma(x,35))
