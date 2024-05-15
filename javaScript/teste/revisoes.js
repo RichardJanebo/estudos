d9017ce17c3af4cf3fcfe401f6eedd997dbe6325
RichardJanebo <richardjanebo@gmail.com>
2024-05-15 07:33:04 -0300

    Revisão 15/02/202

--- javaScript/teste/revisoes.js
+++ javaScript/teste/revisoes.js
@@ -1,10 +1,62 @@
-let p1=[50,40,30,20,10]
+// const texto = document.querySelector("#texto")
+// const botao = document.querySelector("#botao")
 
-let x = p1[p1.length -1]
-console.log(x)
 
-const soma = (x,y)=>{
-  return x + y
-}
 
-console.log(soma(x,35))
+
+// botao.addEventListener("click",()=>{
+//   let x = Number(texto.value.replace(/,/g,"."))
+//   document.querySelector("#res").innerHTML=x
+// })
+
+// // botao.addEventListener("click",({target})=>{
+// //   console.log(target)
+// //   navigator.clipboard.writeText(texto.value)
+// //   let x = parseInt(Number(texto.value))
+// //   x % 2 == 0?alert(`Esse numero é par`):alert("esse numero eé impar")
+// //   salveLocaleStorage(x)
+  
+// // })
+
+// // const salveLocaleStorage =()=>{
+// //     localStorage.setItem("Numero",texto.value)
+
+// // }
+// // function somar(x){
+// //   return x +10
+// // }
+// // const promessa  = new Promise((resolve,reject)=>{
+// //   resolve(
+// //     setTimeout(somar,2000)
+// //   )
+// //   reject("Não conseguimos puxar o resultado")
+
+
+// // })
+// // .then((resultado)=>{
+// //   console.log(resultado)
+// // })
+// // .catch((resultado)=>{
+// //   console.log(resultado)
+// // })
+
+
+// const string = "Ola bom dia"
+
+// console.log(string.charAt(0)) // Retorna o caractere do indice
+// console.log(string.charCodeAt(0)) // Retorna o codigo da letra do indice 0
+// console.log(string.slice(0,5)) //Recorta  a string de um indice no outro 
+
+
+// const set = new Set()
+// set.add("Ola")
+// set.add("Bom dia")
+// set.add("Ola")
+
+// console.log(set)
+
+// let nome = "Maria dos Santos"
+// console.log(nome.search(/o/ig))
+
+const numero = 1.01
+console.log(Math.pow(numero,3))
