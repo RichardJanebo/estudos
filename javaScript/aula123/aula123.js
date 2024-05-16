56f45c298ff1100910922eccf39a2b56673d6cd3
RichardJanebo <richardjanebo@gmail.com>
2024-05-16 08:02:35 -0300

    Aprendendo sobre o metodo post

--- javaScript/aula123/aula123.js
+++ javaScript/aula123/aula123.js
@@ -4,19 +4,12 @@ const p_press = document.querySelector("#p_press")
 const btn_texto = document.querySelector("#btn_texto")
 
 
-
-
-const obterDados = ()=>{
 fetch("https://abd01f3c-3d08-44b0-91ab-6c7883d2cec2-00-2vpt2brpi66ct.kirk.replit.dev/")
-.then(res=>res.json())
-.then((data)=>{
-    p_nivel.innerHTML=`Nivel:${data.nivel }`
-    p_press.innerHTML = `Pressão:${data.pressao}`
-    p_temp.innerHTML= `Temperatura:${data.temperatura}`
+.then((resposta)=>{ return resposta.json()}
+)
+.then((resposta)=>{
+    console.log(resposta)
+    p_temp.innerHTML=`Temperatura ${resposta.temperatura}`
+    p_press.innerHTML=`Pressão ${resposta.pressao}`
+    p_nivel.innerHTML=`Nivel ${resposta.nivel}`
 })
-}
-
-
-
-
-setInterval(obterDados,3000)
---
+++ javaScript/aula124/aula124.js
@@ -0,0 +1,42 @@
+const p_temp = document.querySelector("#p_temp")
+const p_nivel = document.querySelector("#p_nivel")
+const p_press = document.querySelector("#p_press")
+const btn_texto = document.querySelector("#btn_texto")
+
+let endPoint = "https://abd01f3c-3d08-44b0-91ab-6c7883d2cec2-00-2vpt2brpi66ct.kirk.replit.dev/"
+// fetch(endPoint)
+// .then((resposta)=>{ return resposta.json()}
+// )
+// .then((resposta)=>{
+//     console.log(resposta)
+//     p_temp.innerHTML=`Temperatura ${resposta.temperatura}`
+//     p_press.innerHTML=`Pressão ${resposta.pressao}`
+//     p_nivel.innerHTML=`Nivel ${resposta.nivel}`
+// })
+
+let pessoa = {
+    nome:"Richard",
+    idade:20,
+    cor:"Parda"
+}
+
+
+
+let cabecalho = {
+    method:"POST",
+    body:JSON.stringify(pessoa)
+}
+
+function post(){
+    fetch("https://abd01f3c-3d08-44b0-91ab-6c7883d2cec2-00-2vpt2brpi66ct.kirk.replit.dev/",{method:"Post",body:JSON.stringify(pessoa)})
+    .then((resposta)=>{
+        return resposta.json()
+    })
+    .then((resposta)=>{
+        console.log(resposta)
+    })
+}
+
+btn_texto.addEventListener("click",()=>{
+    post()
+})
