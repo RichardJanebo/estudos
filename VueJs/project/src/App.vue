3a1d0ed004926cd1cdb844303e9d56759f0d7048
RichardJanebo <richardjanebo@gmail.com>
2024-05-22 07:58:00 -0300

    v-if e v-show

---
+++ VueJs/project/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <TheHeader v-if="showHeader"></TheHeader>
+  <div>
+     <p v-show="showPara">Nome : {{ name }}</p> 
+     <p >Sobrenome: {{ sobrenome }}</p> 
+  </div> 
+  <div v-if="assesLevel == 1" >Admin</div>
+  <div v-else-if="assesLevel == 2" >Marketing</div>
+  <div v-else-if="assesLevel == 3" >user </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from  './components/TheHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TheHeader
+  },
+  data(){
+  return{
+    showHeader: true,
+    showPara:true,
+    name:"Richard",
+    sobrenome:"Janebo",
+    assesLevel:3,
+  }
+}
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ VueJs/project/src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="h">
+        Header
+    </div>
+</template>
+<script>
+    export default{
+
+    }
+</script>
+
+<style>
+    .h{
+        color: black;
+        font-weight: bold;
+    }
+
+</style>
