40c1154ea5e163d423b6ba802d10df18acd995e8
RichardJanebo <richardjanebo@gmail.com>
2024-05-28 07:30:14 -0300

    diretiva v-for

--- VueJs/aula-01/app/src/components/TheHeader.vue
+++ VueJs/aula-01/app/src/components/TheHeader.vue
@@ -1,17 +1,61 @@
 <template>
     <div class="header">
-        Header
+        <div v-for="ele in todos" 
+        v-bind:key="ele.id" class="todoItem"> {{ ele.title }}</div>
     </div>
 </template>
 <script>
     export default{
+        data(){
+            return{
+                todos :[
+                        {
+                            "userId": 1,
+                            "id": 1,
+                            "title": "delectus aut autem",
+                            "completed": false
+                        },
+                        {
+                            "userId": 1,
+                            "id": 2,
+                            "title": "quis ut nam facilis et officia qui",
+                            "completed": false
+                        },
+                        {
+                            "userId": 1,
+                            "id": 3,
+                            "title": "fugiat veniam minus",
+                            "completed": false
+                        },
+                        {
+                            "userId": 1,
+                            "id": 4,
+                            "title": "et porro tempora",
+                            "completed": true
+                        },
+                        {
+                            "userId": 1,
+                            "id": 5,
+                            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+                            "completed": false
+                        },
+                    ]
 
+
+            }
+        }
     }
 
 
 
 </script>
 <style>
+    .todoItem{
+        background-color:aquamarine ;
+        margin: 0.2rem;
+        padding: 1rem;
+
+    }
     .header{
         font-family: Arial, Helvetica, sans-serif;
         font-weight: bolder;
