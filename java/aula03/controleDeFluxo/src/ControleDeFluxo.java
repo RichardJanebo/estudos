3b8fd0dd0c39061e40e7b276844381c90e7b4296
RichardJanebo <richardjanebo@gmail.com>
2024-05-31 08:01:01 -0300

    Controle de fluxo

---
+++ java/aula03/controleDeFluxo/src/ControleDeFluxo.java
@@ -0,0 +1,12 @@
+public class ControleDeFluxo {
+    public static void main(String[] args) throws Exception {
+        
+        
+        for(int i = 1 ;i < 10 ;i++){
+            System.out.println(i);
+        }
+
+
+
+    }
+}
---
+++ java/aula03/controleDeFluxo/src/ExemploFor.java
@@ -0,0 +1,12 @@
+public class ExemploFor {
+    public static void main(){
+        String alunos [] = {"FELIPE","MATHEUS","LUCAS","JULIA"};
+
+    for (int x=0;alunos.length > x;x++){
+        System.out.println("Nome: " + alunos[x]);
+    }
+
+    }
+    
+
+}
