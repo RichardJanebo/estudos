29d6b8a357a8c3efcebd3ebb3367dfb1a3e16056
RichardJanebo <richardjanebo@gmail.com>
2024-06-23 19:47:02 -0300

    Revisão 23-06-24

---
+++ java/aula16/CostumerExamples.java
@@ -0,0 +1,22 @@
+package aula16;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+
+public class CostumerExamples {
+    public static void main(String[] args){
+        List<Integer> numeros= Arrays.asList(1,2,3,4,5);
+
+        Consumer<Integer> imprimirNumeroPar = numero -> {
+            if(numero % 2 ==0){
+                System.out.println(numero);
+            }
+        };
+
+
+        numeros.stream().forEach(imprimirNumeroPar);
+    }
+}
