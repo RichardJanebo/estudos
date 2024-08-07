6482db86533c3561810385991dfd2ba497f4a3ab
RichardJanebo <richardjanebo@gmail.com>
2024-08-07 07:59:15 -0300

    Sobreescrita de metodos

---
+++ java/devdojo/javacore/Isobrescrita/test/AnimeTeste01.java
@@ -0,0 +1,13 @@
+package devdojo.javacore.Isobrescrita.test;
+
+import devdojo.javacore.Isobrescrita.diminio.Anime;
+
+public class AnimeTeste01 {
+    public static void main(String[] args) {
+        
+        Anime anime = new Anime("Dragon ball Z");
+        System.out.println(anime);
+
+        
+    }
+}
