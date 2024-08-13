6f59d0be64186a2bb970b6bff73b37e899133452
RichardJanebo <richardjanebo@gmail.com>
2024-08-13 08:01:01 -0300

    Finalizando enun

---
+++ java/devdojo/javacore/Kenun/dominio/TipoPagamento.java
@@ -0,0 +1,23 @@
+package devdojo.javacore.Kenun.dominio;
+
+public enum TipoPagamento {
+    CREDITO {
+
+        @Override
+        public double calcularDesconto(double valor) {
+            return valor * 0.1;
+        }
+
+    },
+    DEBITO {
+
+        @Override
+        public double calcularDesconto(double valor) {
+            return valor * 0.1;
+        }
+
+    };
+
+    public abstract double calcularDesconto(double valor);
+
+}
