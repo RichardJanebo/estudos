ee7e9d4caaacb65064e4e958ebb1491209d1c12a
RichardJanebo <richardjanebo@gmail.com>
2024-09-10 08:03:22 -0300

    ChronoUnit

---
+++ java/devdojo/javacore/Rdatas/teste/ChronoUnitTest01.java
@@ -0,0 +1,35 @@
+package devdojo.javacore.Rdatas.teste;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalField;
+
+public class ChronoUnitTest01 {
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.of(2000, Month.APRIL, 6);
+
+        Instant instant = Instant.now();
+
+        LocalTime localTime = LocalTime.now();
+        System.out.println(localTime);
+    
+
+
+
+
+        // System.out.println(ChronoUnit.HOURS.between(localDate,instant ));
+
+
+
+        
+        System.out.println(ChronoUnit.DAYS.between(localDate, LocalDateTime.now()));
+
+        System.out.println(ChronoUnit.MONTHS.between(localDate, LocalDate.now()));
+
+
+    }
+}
---
+++ java/devdojo/javacore/Rdatas/teste/PeriocTest01.java
@@ -0,0 +1,33 @@
+package devdojo.javacore.Rdatas.teste;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
+public class PeriocTest01 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        LocalDate nowAfterTwoYears = LocalDate.now().plusYears(20).plusDays(6);
+
+        Instant instant01 = Instant.now();
+
+        System.out.println(instant01.getNano());
+
+        Instant instant02 = Instant.now().plusSeconds(3);
+
+        Duration duration = Duration.between(instant01, instant02);
+
+        Period p3 = Period.ofWeeks(60);
+
+        System.out.println(duration);
+
+        System.out.println(Period.between(now, now.plusDays(p3.getDays())));
+
+        System.out.println(now.until(now.plusDays(p3.getDays()), ChronoUnit.DAYS));
+
+        ;
+
+    }
+}
