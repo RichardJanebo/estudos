ffcaba9e7da65e5a9cd0a5c6a8c7dfe3edfc99c0
RichardJanebo <richardjanebo@gmail.com>
2024-09-18 08:04:32 -0300

    Zone

---
+++ java/devdojo/javacore/Rdatas/teste/TemporalAdjustersTeste.java
@@ -0,0 +1,19 @@
+package devdojo.javacore.Rdatas.teste;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+
+public class TemporalAdjustersTeste {
+
+   
+
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.now().with(TemporalAdjusters.firstDayOfMonth());
+        System.out.println(localDate);
+
+    }
+}
