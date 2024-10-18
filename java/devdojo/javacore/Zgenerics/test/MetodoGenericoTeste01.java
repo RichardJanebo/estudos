3e0ada9f5223c702f1f211aaf08e0397ad4a8c22
RichardJanebo <richardjanebo@gmail.com>
2024-10-18 08:06:45 -0300

    Classes anonimas

---
+++ java/devdojo/javacore/Zgenerics/test/MetodoGenericoTeste01.java
@@ -0,0 +1,26 @@
+package devdojo.javacore.Zgenerics.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import devdojo.javacore.Zgenerics.dominio.Carro;
+import devdojo.javacore.Zgenerics.dominio.Barco;
+
+public class MetodoGenericoTeste01 {
+    public static void main(String[] args) {
+    //  criarArrayComUmObjeto(new Barco("Canoa marota"));
+    //  criarArrayComUmObjeto(new Carro("Corsa"));
+
+
+
+
+
+
+    }
+
+
+    // private static <T extends Comparable <T>>  List<T>  criarArrayComUmObjeto(T t){
+    //    return List.of(t);
+    // }
+    
+}
