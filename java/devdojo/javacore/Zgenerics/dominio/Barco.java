7464ee62128c4157afa621683366b0ad2f7c68d0
RichardJanebo <richardjanebo@gmail.com>
2024-10-24 08:04:34 -0300

    Streams 01

--- java/devdojo/javacore/Zgenerics/dominio/Barco.java
+++ java/devdojo/javacore/Zgenerics/dominio/Barco.java
@@ -3,6 +3,10 @@ package devdojo.javacore.Zgenerics.dominio;
 public class Barco {
     private String nome;
 
+    public static void metodoStaticoTeste01(){
+        System.out.println("Dentro do metodo estatico");
+    }
+
     public Barco(String nome) {
         this.nome = nome;
     }
--- java/teste09/test/Teste.java
+++ java/teste09/test/Teste.java
@@ -2,63 +2,40 @@ package teste09.test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-import devdojo.javacore.Aintroducaoclasses.exercicio.carro.Carro;
+import devdojo.javacore.Zgenerics.dominio.Barco;
+
+
 
 public class Teste {
     public static void main(String[] args) {
-        {
-            System.out.println("No Bloco de inicialização");
-        }
-        List<String> carros = new ArrayList<>();
-        carros.add("Camaro");
-        carros.add("Fusion");
-        carros.add("Corola");
-        carros.add("Maverick");
-        carros.add("Landal");
+    
+        List<Barco> barcos = new ArrayList<>(List.of(new Barco("Lancha"),new Barco("Iate"),new Barco("Navio")));
+
+        System.out.println(temNaLista(barcos, "Iaete"));
+    
 
 
-        List<Integer> idades = new ArrayList<>();
-        idades.add(10);
-        idades.add(16);
-        idades.add(30);
-        idades.add(35);
-        idades.add(50);
-
-        idades.forEach( e -> System.out.println(e >= 18 ? e :"não " + e) );
-
-        System.out.println("--------------------");
-
-        Function<Integer,String> integerToString = e ->  e.toString();
-
-        System.out.println("---------------------");
-
-        List<String> idade = new ArrayList<>();
         
-        
-        for(int n : idades){
-            idade.add(integerToString.apply(n));
-        }
-
-        System.out.println(idade);
-
 
 
 
     }
 
-    public static <T> List<T> filter(List<T> lista, Predicate<T> predicate) {
-        List<T> outherList = new ArrayList<>();
-        for (T t : lista) {
-            if (predicate.test(t)) {
-                outherList.add(t);
+    public static Optional<String> temNaLista(List<Barco> lisa,String nome){
+        String teste = null;
+        for(Barco b : lisa){
+            if(b.getNome().equals(nome)){
+                teste = b.getNome() + b.toString();
             }
-        }
 
-        return outherList;
+        }
+        return Optional.ofNullable(teste);
     }
+
 
 }
