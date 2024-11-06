8b838bec18d4f830c14db26c2baa207090e1f904
RichardJanebo <richardjanebo@gmail.com>
2024-11-06 08:03:41 -0300

    Wait and notify

---
+++ java/devdojo/javacore/ZZFthreads/test/EmailDeleiveryTest01.java
@@ -0,0 +1,30 @@
+package devdojo.javacore.ZZFthreads.test;
+
+import javax.swing.JOptionPane;
+
+import devdojo.javacore.ZZFthreads.dominio.Members;
+import devdojo.javacore.ZZFthreads.service.EmailDeliveryService;
+
+public class EmailDeleiveryTest01 {
+    public static void main(String[] args) {
+        Members members = new Members();
+
+        Thread jiraia = new Thread(new EmailDeliveryService(members), "Jiraia");
+        Thread kakashi = new Thread(new EmailDeliveryService(members), "Kakashhi");
+
+        jiraia.start();
+        kakashi.start();
+
+        while (true) {
+            String email = JOptionPane.showInputDialog("Entre com o seu email");
+            if(email == null || email.isEmpty()){
+                members.close();
+                break;
+            }
+
+            members.addMemberEmail(email);
+        }
+
+
+    }
+}
