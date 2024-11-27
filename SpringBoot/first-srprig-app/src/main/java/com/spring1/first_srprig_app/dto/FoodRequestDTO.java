a685e647c496c2ab3b05133324e373af10458684
RichardJanebo <richardjanebo@gmail.com>
2024-11-27 07:53:55 -0300

    Alteração de pastas

--- SpringBoot/first-srprig-app/src/main/java/com/spring1/first_srprig_app/dto/FoodRequestDTO.java
+++
@@ -1,5 +0,0 @@
-package com.spring1.first_srprig_app.dto;
-
-public record FoodRequestDTO(String title,String image, Integer price){
-
-}
--- SpringBoot/first-srprig-app/src/main/java/com/spring1/first_srprig_app/repository/FoodReposytory.java
+++
@@ -1,9 +0,0 @@
-package com.spring1.first_srprig_app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring1.first_srprig_app.food.Food;
-
-public interface FoodReposytory extends JpaRepository<Food,Long> {
-    
-}
