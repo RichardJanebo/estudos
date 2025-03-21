8048653ae9812310224088342143dbcacb307df6
RichardJanebo <richardjanebo@gmail.com>
2025-03-21 16:29:45 -0300

    feat: add ProducerRepository.java

---
+++ SpringBoot/anime-service/src/main/java/com/devdojo/repository/ProducerRepository.java
@@ -0,0 +1,40 @@
+package com.devdojo.repository;
+
+import com.devdojo.domain.Producer;
+import com.devdojo.dto.dtoPostRequest.ProducerPostRequest;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+
+@Repository
+public class ProducerRepository {
+    private final ProducerData producerData;
+
+    public  ProducerRepository(ProducerData producerData){
+        this.producerData = producerData;
+    }
+
+
+    public List<Producer> findAll() {
+        return producerData.getProducers().stream().toList();
+    }
+
+    public List<Producer> findByName(List<String> names) {
+        return producerData.getProducers().stream().filter(Producer -> names.contains(Producer.getName())).toList();
+    }
+
+    public Optional<Producer> findById(Long id) {
+        return producerData.getProducers().stream().filter(Producer -> Objects.equals(Producer.getId(), id)).findFirst();
+    }
+
+    public Producer save(Producer producer){
+
+        producerData.getProducers().add(producer);
+        return producer;
+    }
+
+    public void  delete(Producer producer){
+        producerData.getProducers().remove(producer);
+    }
+}
