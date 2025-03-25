e3d81089e3c544f4c8b293e52128a3a0ed29d1c7
RichardJanebo <richardjanebo@gmail.com>
2025-03-25 13:50:08 -0300

    fix: update Anime domain model

--- SpringBoot/anime-service/src/main/java/com/devdojo/domain/Anime.java
+++ SpringBoot/anime-service/src/main/java/com/devdojo/domain/Anime.java
@@ -10,31 +10,31 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Anime {
     private static final Logger log = LoggerFactory.getLogger(Anime.class);
     @EqualsAndHashCode.Include
-    private Long id;
-    private String name;
+    private final Long id;
+    private final String name;
 
     private Anime(Long id, String name) {
         this.id = id;
-        this.name= name;
+        this.name = name;
     }
 
-    public Long getId(){
-        return  this.id;
+    public Long getId() {
+        return this.id;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
-    public static  Builder builder(){
-        return  new Builder();
+    public static Builder builder() {
+        return new Builder();
     }
 
-    public static class Builder{
+    public static class Builder {
         private Long id;
         private String name;
 
-        public Builder id(Long id){
+        public Builder id(Long id) {
             this.id = id;
             return this;
         }
@@ -46,8 +46,8 @@ public class Anime {
 
 
         public Anime build() {
-            log.info("value id dentro do anime '{}'" ,this.id);
-            long l =  this.id != null ? this.id : ThreadLocalRandom.current().nextLong(0, 100);
+            log.info("value id dentro do anime '{}'", this.id);
+            long l = this.id != null ? this.id : ThreadLocalRandom.current().nextLong(0, 100);
             return new Anime(l, name);
         }
     }
